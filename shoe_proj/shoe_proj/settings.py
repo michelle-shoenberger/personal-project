43440589444e0ec5f0d5dc842e4d4dbd91ea3e48
mickey <shoey25ms@gmail.com>
2023-04-11 17:11:03 -0400

    v4 deploy

--- shoe_proj/shoe_proj/settings.py
+++ shoe_proj/shoe_proj/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['django']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['http://localhost:5173','*']
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 # Application definition
 
