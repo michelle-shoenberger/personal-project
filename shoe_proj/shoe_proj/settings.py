89ef318ede8aa7654dcef92714a162053afe0051
mickey <shoey25ms@gmail.com>
2023-04-14 15:41:06 -0400

    updated settings

--- shoe_proj/shoe_proj/settings.py
+++ shoe_proj/shoe_proj/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*']
 
 # Application definition
 
